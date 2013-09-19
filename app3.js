73fbad7dfc2fd8c0a6c0d32113facdb9f067acd9
John Kodumal <jkodumal@atlassian.com>
2013-09-19 12:34:34 -0700

    Test

--- app3.js
+++ app3.js
@@ -1,31 +1,3 @@
 var x = function ( x,y ) {
   return 3;
 };
-
-var y = function(x,z) {
-	return 4;
-};
-
-var z = function(a,b) {
-	return 3;
-};
-
-var xx = function ( x,y ) {
-  return 3;
-};
-
-var zz = function ( x, y ) {
-  return 4;
-};
-
-var xz = function (x,y) {
-  return 4;
-};
-
-var zzz = function ( x, y ) {
-  return 4;
-};
-
-var xzz = function (x,y) {
-  return 4;
-};
